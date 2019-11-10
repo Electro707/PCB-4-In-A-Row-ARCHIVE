b2d1dc5e4f6b10bad220a5a8bb3458be40a04530
Jamal Bouajjaj <jamalnetemail@gmail.com>
2019-11-10 02:27:58 -0500

    Removed TheGame_1 as a submodule.

---
+++ Program - AC6/AC6 Workbench/TheGame_1/Debug/src/subdir.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/main.c \
+../src/syscalls.c \
+../src/system_stm32f10x.c 
+
+OBJS += \
+./src/main.o \
+./src/syscalls.o \
+./src/system_stm32f10x.o 
+
+C_DEPS += \
+./src/main.d \
+./src/syscalls.d \
+./src/system_stm32f10x.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft -DSTM32 -DSTM32F1 -DSTM32F103RCTx -DDEBUG -DSTM32F10X_HD -DUSE_STDPERIPH_DRIVER -I"F:/Projects/Connect4 Board/STM Code/AC6 Workbench/TheGame_1/StdPeriph_Driver/inc" -I"F:/Projects/Connect4 Board/STM Code/AC6 Workbench/TheGame_1/Libraries" -I"F:/Projects/Connect4 Board/STM Code/AC6 Workbench/TheGame_1/inc" -I"F:/Projects/Connect4 Board/STM Code/AC6 Workbench/TheGame_1/CMSIS/device" -I"F:/Projects/Connect4 Board/STM Code/AC6 Workbench/TheGame_1/CMSIS/core" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Program - AC6/AC6 Workbench/TheGame_1/Libraries/delay.h
@@ -0,0 +1,16 @@
+/*
+ * delay.h
+ *
+ *  Created on: Oct 20, 2019
+ *      Author: Electro707
+ */
+
+#ifndef DELAY_H_
+#define DELAY_H_
+
+#include "stm32f10x.h"
+
+void delaySetup(void);
+void delayMs(int ms);
+
+#endif /* DELAY_H_ */
